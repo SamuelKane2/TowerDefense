b3c0cb4b61e2e92c49b91dfd11b9d169151e0882
SamuelKane2 <slkane3@icloud.com>
2024-09-17 13:15:17 -0400

    Pathing

---
+++ scripts/man_green.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+@export var speed = 100
+
+func _process(delta: float) -> void:
+	get_parent().set_progress(get_parent().get_progress() + speed * delta)
+	if get_parent().get_progress_ratio() == 1:
+		queue_free()
