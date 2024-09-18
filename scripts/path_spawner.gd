6e7b07b417bff9c2548b223db297e50cccc87dd9
SamuelKane2 <slkane3@icloud.com>
2024-09-18 13:14:47 -0400

    Creating things

--- scripts/path_spawner.gd
+++ scripts/path_spawner.gd
@@ -2,6 +2,6 @@ extends Node2D
 
 @onready var path = preload("res://scenes/Stage_1.tscn")
 
-func _on_timer_timeout() -> void:
-	var tempPath = path.instatiate()
+func _on_timer_timeout():
+	var tempPath = path.instantiate()
 	add_child(tempPath)
