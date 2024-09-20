f141574ab8635f0704a3d27f2ecef3a1a3b4d2f6
SamuelKane2 <slkane3@icloud.com>
2024-09-20 11:46:07 -0400

    Creating tower mechanism

--- scripts/red_turret.gd
+++ scripts/red_turret.gd
@@ -1,2 +1,15 @@
 extends StaticBody2D
 
+var Bullet = preload("res://scenes/BigRocket.tscn")
+var bulletDamange = 5
+var pathName
+var currTargets = []
+var curr
+
+func _on_tower_body_entered(body:Node2D) -> void:
+	if "ManGreen" in body.name:
+		var tempArray = []
+		currTargets = get_node("Tower").get_overlapping_bodies()
+
+func _on_tower_body_exited(body:Node2D) -> void:
+	pass # Replace with function body.
