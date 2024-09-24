7b0d079bed577057b011edb14bee01183f16d4a6
SamuelKane2 <slkane3@icloud.com>
2024-09-24 16:59:21 -0400

    Homing missiles, Rotating Towers

--- scripts/red_turret.gd
+++ scripts/red_turret.gd
@@ -6,13 +6,38 @@ var pathName
 var currTargets = []
 var curr
 
+func _process(delta):
+	if is_instance_valid(curr):
+		self.look_at(curr.global_position)
+	else:
+		for i in get_node("BulletContainer").get_child_count():
+			get_node("BulletContainer").get_child(i).queue_free()
+
 func _on_tower_body_entered(body: Node2D) -> void:
 	if body.name == "GreenMan":
 		var tempArray = []
 		currTargets = get_node("Tower").get_overlapping_bodies()
+		
 		for i in currTargets:
 			if "GreenMan" in i.name:
 				tempArray.append(i)
+		var currTarget = null
+		
+		for i in tempArray:
+			if currTarget == null:
+				currTarget = i.get_node("../")
+			else:
+				if i.get_parent().get_progress() > currTarget.get_progress():
+					currTarget = i.get_node("../")
+		
+		curr = currTarget
+		pathName = currTarget.get_parent().name
+		
+		var tempBullet = Bullet.instantiate()
+		tempBullet.pathName = pathName
+		tempBullet.bulletDamage = bulletDamange
+		get_node("BulletContainer").add_child(tempBullet)
+		tempBullet.global_position = $Aim.global_position
 		
 func _on_tower_body_exited(body):
-	pass
+	currTargets = get_node("Tower").get_overlapping_bodies()
