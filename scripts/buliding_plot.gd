3e1f185706ab05dcc89a0071981c2d5c505185d4
SamuelKane2 <slkane3@icloud.com>
2024-09-28 14:12:13 -0400

    Horrible debugging

---
+++ scripts/buliding_plot.gd
@@ -0,0 +1,13 @@
+extends Panel
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_gui_input(event: InputEvent) -> void:
+	print("Click on")
+	# I dream to get this working someday
--- scripts/red_turret_panel.gd
+++ scripts/red_turret_panel.gd
@@ -1,10 +1,28 @@
 extends Panel
 
-#@onready var tower = preload("res://scenes/BigRocket.tscn")
-#var currTile
+@onready var tower = preload("res://scenes/RedTurret.tscn")
+var currTile
 
 func _on_gui_input(event:InputEvent) -> void:
-	print(event)
-	#var tempTower = tower.instantiate()
-	#if event is InputEventMouseButton and event.button_mask == 1:
-		#pass
+	var tempTower = tower.instantiate()
+	tempTower.get_node("Area").visible = true
+	if event is InputEventMouseButton and event.button_mask == 1 and event.button_index == 1:
+		# Left Click Down
+		add_child(tempTower)
+		tempTower.global_position = event.global_position
+
+		
+	elif event is InputEventMouseMotion and event.button_mask == 1:
+		# Left click Down Drag
+		get_child(3).global_position = event.global_position
+		
+	elif event is InputEventMouseButton and event.button_mask == 0 and event.button_index == 1:
+		# Left Click Up
+		if event.global_position.x >= 1000:
+			get_child(3).queue_free()
+		else:
+			var path = get_tree().get_root().get_node("Game/Towers")
+			get_child(3).queue_free()
+			path.add_child(tempTower)
+			tempTower.global_position = event.global_position
+			tempTower.get_node("Area").visible = false
