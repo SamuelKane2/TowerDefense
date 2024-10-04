68fa6ceb70ef545ec38aab70062cd056b62672d0
SamuelKane2 <slkane3@icloud.com>
2024-10-03 20:21:15 -0400

    Restart

--- scripts/death_screen.gd
+++ scripts/death_screen.gd
@@ -14,11 +14,27 @@ func _on_button_pressed() -> void:
 			if "EnemyTimer" not in i.name:
 				queue_free()
 				
-	
+@onready var death_timer: Timer = $DeathTimer
+			
 @onready var warning_timer: Timer = $"../../WarningTimer"
 @onready var beginning_timer: Timer = $"../../BeginningTimer"
 
 func _process(delta):
-	if Game.health <= 0:
+	if Game.health <= 19:
 		death_screen.visible = true
 		get_tree().paused = true
+
+func _on_death_timer_timeout() -> void:
+	print("Death Timer timeout")
+	get_tree().paused = false
+	death_screen.visible = false
+	warning_label.visible = true
+	warning_panel.visible = true
+	warning_timer.start(5)
+	for i in get_tree().get_child(1).get_children():
+			if "EnemyTimer" not in i.name:
+				i.queue_free()
+				
+				
+				
+				
